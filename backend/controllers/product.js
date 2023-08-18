275febfae824f104c048bd012f7812813f441128
taahirahismail <taahirah.i@gmail.com>
2023-08-18 10:25:28 +0200

    backend done

---
+++ backend/controllers/product.js
@@ -0,0 +1,61 @@
+// import function from product model
+import {getProducts, getProductById, insertProduct, updateProductById, deleteProductById} from "../models/productModel.js";
+
+// get all products
+export const showProducts = (req, res) => {
+    getProducts((err, results) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json(results);
+        }
+    });
+}
+
+// get single product
+export const showProductById = (req, res) => {
+    getProductById(req.params.id, (err, results) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json(results);
+        }
+    });
+}
+
+// create new product
+export const createProduct = (req, res) => {
+    const data = req.body;
+    insertProduct(data, (err, results) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json(results);
+        }
+    });
+}
+
+// update product
+export const updateProduct = (req, res) => {
+    const data = req.body;
+    const id = req.params.id;
+    updateProductById(data, id, (err, results) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json(results);
+        }
+    });
+}
+
+// delete product
+export const deleteProduct = (req, res) => {
+    const id = req.params.id;
+    deleteProductById(id, (err, results) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json(results);
+        }
+    });
+}
